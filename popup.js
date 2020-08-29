5bd55447658f95b6e3a14b42ab3316ae18edccfd
Lahari <lahari@Laharis-MacBook-Pro.local>
2020-08-29 15:17:52 -0700

    Worked on syncing the input text box with the pop up to display what is inputted into the text box under the summarized text section.

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
-$(function() {
-    $('#submitText').click(function(){
-        chrome.storage.sync.get('text', function(inputText){
-            var submitted_text = $('#inputText').val();
+$(function(){
+ 
+    $('#submitTextPop').click(function(){
+         chrome.storage.sync.get('summarizedTextPop', function(summarize){
+             var newSummary = "";
+             var inputText = $('#inputTextPop').val();
+             newSummary += inputText;
+   
+             chrome.storage.sync.set({'summarizedTextPop': newSummary}, function() {});
             
-            
+             $('#summarizedTextPop').text(newSummary);
+             $('#inputTextPop').val('');
+   
+         })
+        
     })
-})
-
-})
+   
+  });
+  
