39429387f536c6c24f171f95701e47d330aa215b
Lahari <lahari@Laharis-MacBook-Pro.local>
2020-08-29 15:20:20 -0700

    Added similar storage sync features and buttons to the options page just like those in the popup, so what is inputted into the text box is now displayed under the summarized text section.

--- options.js
+++ options.js
@@ -1,11 +1,15 @@
 $(function(){
-    $('#saveText').click(function(){
-        var text = $('#inputText').val();
-        if(text) {
-            chrome.storage.sync.set({'text': text}, function() {
-                close();
-            });
-        }
-    });
-    $('')
-})
+ 
+    $('#submitTextOp').click(function(){
+         chrome.storage.sync.get('summarizedTextOp', function(summarize){
+             var newSummary = "";
+             var inputText = $('#inputTextOp').val();
+             newSummary += inputText;
+             chrome.storage.sync.set({'summarizedTextOp': newSummary}, function() {});
+            
+             $('#summarizedTextOp').text(newSummary);
+             $('#inputTextOp').val('');
+         })
+        
+    })
+ });
